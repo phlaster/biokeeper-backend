0e8bc7954b44599132096873ef3ede24b12a1a23
kiriklzz <kirill.zabrodin03@gmail.com>
2024-07-08 00:57:10 +0500

    add forbidden exception

--- python/src/exceptions.py
+++ python/src/exceptions.py
@@ -17,8 +17,14 @@ class NoResearchException(NotFoundException):
 class NoKitException(NotFoundException):
     pass
 
+class HTTPForbiddenException(HTTPException):
+    status_code = status.HTTP_403_FORBIDDEN
+    def __init__(self, *args, **kwargs):
+        super().__init__(status_code=self.status_code, *args, **kwargs)
+        
+
 class HTTPNotFoundException(HTTPException):
     status_code = status.HTTP_404_NOT_FOUND
-    def __init__(self, message):
-        super().__init__(status_code=self.status_code, detail=message)
-        
+    def __init__(self, *args, **kwargs):
+        super().__init__(status_code=self.status_code, *args, **kwargs)
+
