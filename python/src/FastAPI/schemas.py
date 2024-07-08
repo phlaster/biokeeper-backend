4e120fcc92575ceccfa5e372f0ddeff935fc7036
catfield123 <gtagta65@gmail.com>
2024-07-08 03:41:29 +0300

    pydantic Identifier class

--- python/src/FastAPI/schemas.py
+++ python/src/FastAPI/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 import datetime
 
 class Role(BaseModel):
@@ -7,6 +7,24 @@ class Role(BaseModel):
     info: str
 
 class TokenPayload(BaseModel):
+    id: int
     username: str
     role : Role
     exp: datetime.datetime
+
+from pydantic import BaseModel, validator, ValidationError
+from typing import Union
+
+class Identifier(BaseModel):
+    identifier: int | str
+
+    @validator('identifier', pre=True)
+    def validate_identifier(cls, v):
+        if isinstance(v, int):
+            return v
+        elif isinstance(v, str):
+            if v.isdigit():
+                return int(v)
+            else:
+                return v
+        raise ValueError('Identifier must be either an integer or a string')
