4a3220680c7708d615f93810fbe104e57b3262be
catfield123 <gtagta65@gmail.com>
2024-07-08 03:39:18 +0300

    typo fix

--- python/src/FastAPI/routers/samples.py
+++ python/src/FastAPI/routers/samples.py
@@ -17,13 +17,13 @@ def get_samples(token_payload: Annotated[TokenPayload, Depends(get_current_user)
     return DBM.samples.get_all()
 
 @router.get('/samples/{sample_id}')
-def get_sample(sample_id, token_payload: Annotated[TokenPayload, Depends(get_current_user)]):
+def get_sample(sample_id: int, token_payload: Annotated[TokenPayload, Depends(get_current_user)]):
     try:
         dbm_sample = DBM.samples.get_info(sample_id)
     except NoSampleException:
-        raise HTTPNotFoundException(details=f'Sample {sample_id} not found')
+        raise HTTPNotFoundException(detail=f'Sample {sample_id} not found')
     if not is_admin(token_payload) and dbm_sample['owner_id'] != token_payload.id or is_observer(token_payload):
-        raise HTTPForbiddenException(details=f'Sample owner differs from autorized user')
+        raise HTTPForbiddenException(detail=f'Sample owner differs from autorized user')
     return dbm_sample
 
 @router.post('/samples')
@@ -40,41 +40,40 @@ def create_sample(
     try:
         dbm_research = DBM.researches.get_info(research_name)
     except NoResearchException:
-        raise HTTPNotFoundException(details=f'Research {research_name} not found')
+        raise HTTPNotFoundException(detail=f'Research with id {research_id} not found')
     
     if dbm_research['approval_required']:
         user_researches = DBM.users.get_user_participated_researches(token_payload.id)
         if not dbm_research['id'] in user_researches:
-            raise HTTPForbiddenException(details=f'User {token_payload.id} does not participate in research {research_name}')
+            raise HTTPForbiddenException(detail=f"User {token_payload.id} does not participate in research {dbm_research['name']}")
         
     if not dbm_research['status'] != 'ongoing':
-        raise HTTPConflictException(details=f'Research {research_name} is not in "ongoing" status.')
+        raise HTTPConflictException(detail=f"Research {dbm_research['name']} is not in \"ongoing\" status.")
 
     try:
         dbm_qr_info = DBM.samples.get_qr_info(qr_hex)
     except NoQrCodeException:
-        raise HTTPNotFoundException(details=f'Qr {qr_hex} not found')
+        raise HTTPNotFoundException(detail=f'Qr {qr_hex} not found')
 
     if dbm_qr_info['is_used']:
-        raise HTTPConflictException(details=f'Qr {qr_hex} is already used')
+        raise HTTPConflictException(detail=f'Qr {qr_hex} is already used')
     
-    if dbm_qr_info['kit_id']:
-        raise HTTPConflictException(details=f'Qr {qr_hex} is not assigned to any kit')
+    if not dbm_qr_info['kit_id']:
+        raise HTTPConflictException(detail=f'Qr {qr_hex} is not assigned to any kit')
     
     try:
         dbm_kit = DBM.kits.get_info(dbm_qr_info['kit_id'])
     except NoQrCodeException:
-        raise HTTPNotFoundException(details=f'No kit with id {dbm_qr_info["kit_id"]} found (very strange).')
+        raise HTTPNotFoundException(detail=f'No kit with id {dbm_qr_info["kit_id"]} found (very strange).')
     
     if not dbm_kit['owner']:
-        raise HTTPForbiddenException(details=f'Kit {dbm_qr_info["kit_id"]} is not assigned to any user')
+        raise HTTPForbiddenException(detail=f'Kit {dbm_qr_info["kit_id"]} is not assigned to any user')
     
     if not dbm_kit['owner']['id'] == token_payload.id:
-        raise HTTPForbiddenException(details=f'User {token_payload.id} does not own kit {dbm_kit["id"]}')
+        raise HTTPForbiddenException(detail=f"User {token_payload.id} does not own kit {dbm_kit['id']}")
     
     if not dbm_kit['status'] == 'activated':
-        raise HTTPConflictException(details=f'Kit {dbm_kit["id"]} hasn\'t been activated')
-
+        raise HTTPConflictException(detail=f'Kit {dbm_kit["id"]} hasn\'t been activated')
 
 
     dbm_new_sample_id = DBM.samples.new(qr_id=dbm_qr_info['id'], 
